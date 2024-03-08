58c044689843e75681c610fc741fe5774f87e54f
Rafael <rafael.leite.14@hotmail.com>
2024-03-08 15:13:28 +0000

    Initial commit

---
+++ element.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+if [[ -z $1 ]]
+then
+  echo Please provide an element as an argument.
+else
+  PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+  #Verify if argument is a number
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1;");
+  else
+    #Verify if argument is a symbol or a name
+    if [[ ${#1} -le 2 ]]; then
+      ELEMENT=$($PSQL "SELECT * FROM elements WHERE symbol='$1';");
+    else
+      ELEMENT=$($PSQL "SELECT * FROM elements WHERE name LIKE '%$1%'");
+    fi
+  fi
+
+  if [[ -z $ELEMENT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    IFS='|' read -ra ELEMENT <<< "$ELEMENT"
+
+    ELEMENT_ATOMIC_NUMBER=${ELEMENT[0]}
+    ELEMENT_SYMBOL=${ELEMENT[1]}
+    ELEMENT_NAME=${ELEMENT[2]}
+
+    PROPERTIES=$($PSQL "SELECT * FROM properties WHERE atomic_number=$ELEMENT_ATOMIC_NUMBER");
+
+    IFS='|' read -ra PROPERTIES <<< "$PROPERTIES"
+  
+    ELEMENT_MELTING_POINT=${PROPERTIES[3]}
+    ELEMENT_BOILING_POINT=${PROPERTIES[4]}
+    ELEMENT_MASS=${PROPERTIES[2]}
+    ELEMENT_TYPE=${PROPERTIES[1]}
+
+    echo "The element with atomic number $ELEMENT_ATOMIC_NUMBER is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ELEMENT_MASS amu. $ELEMENT_NAME has a melting point of $ELEMENT_MELTING_POINT celsius and a boiling point of $ELEMENT_BOILING_POINT celsius."
+  fi
+fi
